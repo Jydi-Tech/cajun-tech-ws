94fa77205e3f2a1a2e168e3adfe0aa7e27b53c8e
jydi <jydi@protonmail.com>
2024-01-17 12:22:08 -0700

    generated components

---
+++ src/app/coding/coding.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-coding',
+  standalone: true,
+  imports: [],
+  templateUrl: './coding.component.html',
+  styleUrl: './coding.component.css'
+})
+export class CodingComponent {
+
+}
---
+++ src/app/networks/networks.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-networks',
+  standalone: true,
+  imports: [],
+  templateUrl: './networks.component.html',
+  styleUrl: './networks.component.css'
+})
+export class NetworksComponent {
+
+}
