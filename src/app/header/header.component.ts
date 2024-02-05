aae0203dcfc93f2facc74ad361a21f58d0ccaf45
jydi <jydi@protonmail.com>
2024-02-05 16:20:57 -0700

    fixed the favicon.ico

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { BUTTON_LABELS, HEADER_BUTTON_CELL_WIDTH_VAR, environment } from '../../environments/environment';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 
 
 @Component({
@@ -12,17 +12,27 @@ import { RouterModule } from '@angular/router';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  
   public buttonLabels : string[] = BUTTON_LABELS;
   public labelPercent : string = HEADER_BUTTON_CELL_WIDTH_VAR;
   headerImage: string = environment.imageUrl + "/CajunTechLogoWhite.png";
   showCopyNotification : boolean = false;
   
+  constructor(private router: Router){  
+    //this is needed to allow the cajunTech image to go to root as it passes in the router object for goToRootPage
+  }
+
+  goToRootPage() {
+    this.router.navigate(['/']);
+  }
+  
   onButtonClick(buttonLabel: string){
+    //this is needed to allow the button to work it passes the string
   }
 
   copyPhoneNumber() {
     const text : string = "3076792101";
-    if (text) {
+    if (text) { //if text is not null
       navigator.clipboard.writeText(text).then(() => {
         this.showCopyNotification = true;
         setTimeout(() => this.showCopyNotification = false, 3000)
