863fbaf6c08e162eadf72de73da316f224c81a62
jydi <jydi@protonmail.com>
2024-02-05 14:48:13 -0700

    before i start changes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -15,7 +15,21 @@ export class HeaderComponent {
   public buttonLabels : string[] = BUTTON_LABELS;
   public labelPercent : string = HEADER_BUTTON_CELL_WIDTH_VAR;
   headerImage: string = environment.imageUrl + "/CajunTechLogoWhite.png";
+  showCopyNotification : boolean = false;
   
   onButtonClick(buttonLabel: string){
+  }
+
+  copyPhoneNumber() {
+    const text : string = "3076792101";
+    if (text) {
+      navigator.clipboard.writeText(text).then(() => {
+        this.showCopyNotification = true;
+        setTimeout(() => this.showCopyNotification = false, 3000)
+        console.log('Phone number copied to clipboard');
+      }).catch(err => {
+        console.error('Failed to copy: ', err);
+      });
+    }
   }
 }
